4347f27a5dd677dd0a31404c8691150c0d7c5a79
gogo <eric27202660@gmail.com>
2022-01-19 03:11:32 +0800

    Initial Commit

---
+++ Booker/BookerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookerApp.swift
+//  Booker
+//
+//  Created by 官鼎鈞 on 2022/1/19.
+//
+
+import SwiftUI
+
+@main
+struct BookerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
